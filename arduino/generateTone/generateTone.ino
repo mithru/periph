7385c45866c89c4a7946cadda8a84264c3a836e6
Mithru Vigneshwara <mithru27@gmail.com>
2016-11-26 12:00:11 +0800

    Add files via upload

---
+++ arduino/generateTone/generateTone.ino
@@ -0,0 +1,29 @@
+String inString = "";
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  while (Serial.available() > 0) {
+    int inChar = Serial.read();
+    if (isDigit(inChar)) {
+      // convert the incoming byte to a char
+      // and add it to the string:
+      inString += (char)inChar;
+    }
+    // if you get a newline, print the string,
+    // then the string's value:
+    if (inChar == '\n') {
+      Serial.print("Value:");
+      int value = inString.toInt();
+      Serial.println(value);
+      Serial.print("String: ");
+      Serial.println(inString);
+      // clear the string for new input:
+      inString = "";
+      tone(8, value);
+    }
+  }
+}
+
